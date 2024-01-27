f6593cdfd0e8d0c8e9acbbfa2b227acd459cd35d
xdd0225 <dingyuzhe0225@gmail.com>
2024-01-27 22:42:34 +0100

    Update camera_robot_tf_publischer.py

--- src/camera_robot_tf/src/camera_robot_tf_publischer.py
+++ src/camera_robot_tf/src/camera_robot_tf_publischer.py
@@ -8,71 +8,29 @@ from geometry_msgs.msg import TransformStamped
 from geometry_msgs.msg import Point
 from camera_robot_tf.calculate_transformmatrix import CalculateTransformMatrix
 
-count = 0
 point_A1, point_A2, point_A3 = None, None, None
 point_B1, point_B2, point_B3 = None, None, None
+point_A,point_B = None, None
 
 def camerapoint_callback(msg):
-    global count, point_A1, point_A2, point_A3
-    print('get the camera point position is:\n', msg)
-    while True:
-        count = input('Please input the number of the CAMERA point:\n')
-        if count in ['1', '2', '3']:
-            count = int(count)
-            break
-        else:
-            print('Please input a valid number (1, 2, or 3).\n')
-
-    if count == 1:
-        if point_A1 is not None:
-            print('The point A1 has been set. Please input another number.\n')
-        else:
-            point_A1 = np.array([msg.x, msg.y, msg.z])
-            print('point_A1 is :\n', point_A1)
-    elif count == 2:
-        if point_A2 is not None:
-            print('The point A2 has been set. Please input another number.\n')
-        else:
-            point_A2 = np.array([msg.x, msg.y, msg.z])
-            print('point_A2 is :\n', point_A2)
-    elif count == 3:
-        if point_A3 is not None:
-            print('The point A3 has been set. Please input another number.\n')
-        else:
-            point_A3 = np.array([msg.x, msg.y, msg.z])
-            print('point_A3 is :\n', point_A3)
+    global point_A
+    point_A = np.array([msg.x, msg.y, msg.z])
 
 
 def robotendpoint_callback(msg):
-    global count, point_B1, point_B2, point_B3
-    print('get the robot endpoint position is:\n', msg.O_T_EE[12:15])
-    while True:
-        count = input('Please input the number of the Robot endpoint:\n')
-        if count in ['1', '2', '3']:
-            count = int(count)
-            break
-        else:
-            print('Please input a valid number (1, 2, or 3).\n')
+    global point_B
+    point_B = np.array(msg.O_T_EE[12:15])
 
-    if count == 1:
-        if point_B1 is not None:
-            print('The point B1 has been set. Please input another number.\n')
-        else:
-            point_B1 = np.array(msg.O_T_EE[12:15])
-            print('point_B1 is :\n', point_B1)
-    elif count == 2:
-        if point_B2 is not None:
-            print('The point B2 has been set. Please input another number.\n')
-        else:
-            point_B2 = np.array(msg.O_T_EE[12:15])
-            print('point_B2 is :\n', point_B2)
-    elif count == 3:
-        if point_B3 is not None:
-            print('The point B3 has been set. Please input another number.\n')
-        else:
-            point_B3 = np.array(msg.O_T_EE[12:15])
-            print('point_B3 is :\n', point_B3)
 
+def robot_subscriber():
+    rospy.init_node('transformation_init_robot', anonymous=True)
+    rospy.Subscriber('/franka_state_controller/franka_states', FrankaState, robotendpoint_callback)
+    rospy.spin()
+
+def camera_subscriber():
+    rospy.init_node('transformation_init_camera', anonymous=True)
+    rospy.Subscriber('/matlabinit', Point, camerapoint_callback, queue_size=1)
+    rospy.spin()
 
 def calculate_and_apply_transformmatrix():
     Init = CalculateTransformMatrix()
@@ -105,27 +63,36 @@ def publish_tf(rotation, translation):
     tfs.transform.rotation.w = quaternion[3]
 
     broadcaster.sendTransform(tfs)
-    ros.spin()
+    rospy.spin()
 
 def main():
+    global point_A1, point_A2, point_A3, point_B1, point_B2, point_B3 ,point_A , point_B
     rospy.init_node('transformation_init', anonymous=True)
-
-    # 订阅相机和机器人的消息
     rospy.Subscriber('/matlabinit', Point, camerapoint_callback, queue_size=1)
     rospy.Subscriber('/franka_state_controller/franka_states', FrankaState, robotendpoint_callback, queue_size=1)
-
-
-    while not rospy.is_shutdown():
-        if point_A1 is not None and point_A2 is not None and point_A3 is not None \
-                and point_B1 is not None and point_B2 is not None and point_B3 is not None:
-            print('Now you can calculate the transformation matrix.')
-            rotation, translation = calculate_and_apply_transformmatrix()
-            print('The rotation matrix is:', rotation)
-            print('The translation matrix is:', translation)
-            publish_tf(rotation, translation)
-            print('The transformation matrix is published.')
-            # rospy.sleep(0.1) # 如果收集到足够的数据
+    rospy.sleep(1)
+    point_A1 = point_A
+    point_B1 = point_B
+    print('The point_A1 is:\n', point_A1)
+    print('The point_B1 is:\n', point_B1)
+    input('Press Enter to confirm the first point and get the next point.')
+    point_A2 = point_A
+    point_B2 = point_B
+    print('The point_A2 is:\n', point_A2)
+    print('The point_B2 is:\n', point_B2)
+    input('Press Enter to confirm the second point and get the next point.')
+    point_A3 = point_A
+    point_B3 = point_B
+    print('The point_A3 is:\n', point_A3)
+    print('The point_B3 is:\n', point_B3)
+    print('Now you can calculate the transformation matrix.')
+    rotation, translation = calculate_and_apply_transformmatrix()
+    print('The rotation matrix is:\n', rotation)
+    print('The translation matrix is:\n', translation)
+    publish_tf(rotation, translation)
+    print('The transformation matrix is published.\n')
 
 
 if __name__ == '__main__':
     main()
+
